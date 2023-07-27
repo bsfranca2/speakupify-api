c4976d5bcb1813a1f7082b294e5101f13a9dd644
bsfranca2 <bsfranca2@gmail.com>
2023-07-27 20:54:52 -0300

    log app port, health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './modules/prisma';
 import { VisitorsModule } from './modules/visitors';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configuration } from './modules/config';
+import { HealthModule } from './modules/health';
 
 @Module({
-  imports: [VisitorsModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    HealthModule,
+    VisitorsModule,
+  ],
   controllers: [],
   providers: [PrismaService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: string | number;
+  static isDev: boolean;
+
+  constructor(private readonly config: ConfigService) {
+    AppModule.port = config.get('PORT') || 3000;
+    AppModule.isDev = config.get('NODE_ENV') !== 'production';
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(AppModule.port);
+  logger.log('Listening at http://localhost:' + AppModule.port + '/');
 }
 bootstrap();
